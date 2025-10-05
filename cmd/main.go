aabc8b9c36ae5532618aa24cee557c76db276514
ninosistemas10 <ninoarangotinco@gmail.com>
2025-10-05 12:51:11 +0200

    commit main

--- cmd/main.go
+++ cmd/main.go
@@ -4,28 +4,39 @@ import (
 	"log"
 	"os"
 
+	"github.com/labstack/echo/v4"
 	"github.com/ninosistemas10/kiosko/infrastructure/handler"
 	"github.com/ninosistemas10/kiosko/infrastructure/handler/response"
+	"github.com/ninosistemas10/kiosko/infrastructure/handler/ws"
 )
 
 func main() {
+	// Cargara variables de entorno
 	if err := loadEnv(); err != nil {
 		log.Fatal(err)
 	}
-
+	//validar variables de entorno
 	err := validateEnvironments()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	//crea servidor HTTP (ECHO O TU wrapper)
 	e := newHTTP(response.HTTPErrorHandler)
-
+	// Crear instancia de dbPool
+	//Crea la base de Datos
 	dbPool, err := newDBConnection()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	//Inicializa las rutas HTTP
 	handler.InitRoutes(e, dbPool)
+
+	hub := ws.NewHub()
+	go hub.Run()   //Inicia el hub en un goroytine
+	ws.SetHub(hub) // Guardar hub globakl para Emit()
+	e.GET("/ws", func(c echo.Context) error { return ws.ServeWS(hub, c) })
 
 	err = e.Start(":" + os.Getenv("SERVER_PORT"))
 	if err != nil {
